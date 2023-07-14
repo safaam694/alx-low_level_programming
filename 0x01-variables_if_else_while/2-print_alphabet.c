ebea6462732056a02627e1e37e99c0423dee0c52
safaam694 <safaagadelrab694@gmail.com>
2023-07-14 14:39:47 -0700

    d

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print the alphabet in lowercase using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+	
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	
+	putchar('\n');
+	
+	return (0);
+}
