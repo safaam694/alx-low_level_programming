7039e2958a693116a2b68fa026a4195eb6b0dd1c
safaam694 <safaagadelrab694@gmail.com>
2023-07-18 13:20:57 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,19 @@
-#include <unistd.h>
-
-int main(void)
-{
-	char s[] = "_putchar\n";
-	int len = sizeof(s) / sizeof(s[0]);
-	int i;
-
-	for (i = 0; i < len; i++)
-	{
-		_putchar(s[i]);
-	}
-	return (0);
-}
+1 #include "main.h"
+2 /**
+3 *main - Entry  point
+4 *Description: 'prints _putchar to stdout'
+5 *Return: always O
+6 */
+7 int main (viod)
+8 {
+9         _putchar('_');
+10        _putchar('p');
+11        _putchar('u');
+12        _putchar('t');
+13        _putchar('c');
+14        _putchar('h');
+15        _putchar('a');
+16        _putchar('r');
+17        _putchar('\n');
+18        return (0);
+19 } 
