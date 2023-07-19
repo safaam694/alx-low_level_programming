56e5b1643ccfc3b7cc5cbd4fc2ebbcef4b079669
safaam694 <safaagadelrab694@gmail.com>
2023-07-19 15:18:10 -0700

    s

--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-raedme
+putchar
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,14 +1,13 @@
-#include <unisttd.h>
-#include "main.h"
+#include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
+ * _putchar - writethe character c to stdout
  * @c: The character to print
- *
  * Return: On success 1.
- *         On error, -1 is returned, and erron is set appropriately
+ * On error, -1 is returned, and errnois set appropriately.
  */
-int _putchar(char c)
+
+int _putchar (char c)
 {
 	return (write(1, &c, 1));
 }
