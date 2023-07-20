8f7f8b60dd56cd83c883b23a3989345923838c6a
safaam694 <safaagadelrab694@gmail.com>
2023-07-20 07:21:45 -0700

    s

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * nul - multiplies two integers
+ * @x: The first number to multiply
+ * @y: The second number to multiply with first number
+ * Return: The result of multiplication between x param and y paream
+ */
+int nul(int x, int y)
+{
+	return (x * y);
+}
