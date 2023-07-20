ca30d8ab57c0eab34beca57941bbf8e887023c42
safaam694 <safaagadelrab694@gmail.com>
2023-07-20 11:08:52 -0700

    d

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square of a given size
+ * @size: the size of the square
+ */
+void print_square(int size)
+{
+    int row, column;
+
+    if (size <= 0) {
+        _putchar('\n');
+        return;
+    }
+
+    for (row = 1; row <= size; row++) {
+        for (column = 1; column <= size; column++) {
+            _putchar('#');
+        }
+        _putchar('\n');
+    }
+}
