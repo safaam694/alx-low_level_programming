7fbb8b641e2f0a2ec8a552cbce8d4d2c18591e8b
safaam694 <safaagadelrab694@gmail.com>
2023-07-24 11:26:22 -0700

    s

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: The string to print
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		putchar(*str);
+	}
+	putchar('\n');
+}
