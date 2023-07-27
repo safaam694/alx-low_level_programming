4bc678d14ff8c28d06160f389a6c0f300f3eee06
safaam694 <safaagadelrab694@gmail.com>
2023-07-27 12:16:18 -0700

    f

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters
+ * of a string to uppercase.
+ * @str: The string to be changed.
+ * return: A pointer to the changed string.
+ */
+
