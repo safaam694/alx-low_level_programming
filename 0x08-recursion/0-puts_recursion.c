609668a4faaefa2f89eaa8e8e2c0d99482c01cc9
safaam694 <safaagadelrab694@gmail.com>
2023-08-02 02:15:19 -0700

    d

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _put_recursion - print a string folowed by a new line
+ * @s: the string to print
+ * 
+ * Return: Nothing.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
