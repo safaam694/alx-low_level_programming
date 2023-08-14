c1244757220262fd2bb5c21f83b61fc8dd8fe0a4
safaam694 <safaagadelrab694@gmail.com>
2023-08-14 16:30:30 -0700

    s

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _OBJECT_LINK_MACRD_H
+#define _OBJECT_LINK_MACRD_H
+
+#define SIZE 1024
+
+#endif /* _OBJECT_LINK_MACRD_H */
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCTION_LINKE_MACRO_H
+#define _FUNCTION_LINKE_MACRO_H
+
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+
+#endif /* _FUNCTION_LINKE_MACRO_H */
